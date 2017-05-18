1ef65b8df1ae7bb18abd8e9d67bf5fa9bc6ae1fe
Henry Johnson <nzhenry@gmail.com>
2017-05-18 08:48:16 -0400

    no message

---
+++ src/main/java/com/mycompany/app/FooBar.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class FooBar 
+{
+    public static void foobar()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
